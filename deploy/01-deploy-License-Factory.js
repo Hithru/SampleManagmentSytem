e69912b0d68951e7537d16c8d0d8fe92b05d82f4
Hithru <hithrudealwis@gmail.com>
2022-10-15 21:02:36 +0530

    Add deploy scripts to use hardhat deploy

---
+++ deploy/01-deploy-License-Factory.js
@@ -0,0 +1,37 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+require("dotenv").config()
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("----------------------------------------------------")
+    log("Deploying LicenseFactory and waiting for confirmations...")
+    const licenseFactory = await deploy("LicenseFactory", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    log(`LicenseFactory deployed at ${licenseFactory.address}`)
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(licenseFactory.address, [ethUsdPriceFeedAddress])
+    }
+}
+
+module.exports.tags = ["all", "licenseFactory"]
